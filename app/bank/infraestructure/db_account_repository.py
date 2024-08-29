8ead0324fc29ea98a6c44f6f749e7a83eed19d9e
antoniahp <a.herrerapuertas@gmail.com>
2024-08-29 10:11:21 +0200

    refactor update for select

--- app/bank/infraestructure/db_account_repository.py
+++ app/bank/infraestructure/db_account_repository.py
@@ -1,5 +1,6 @@
 from decimal import Decimal
 from typing import Optional
+from uuid import UUID
 
 from bank.domain.account import Account
 from bank.domain.account_repository import AccountRepository
@@ -10,8 +11,8 @@ class DbAccountRepository(AccountRepository):
     def get_account_by_iban(self, account_number: str, select_for_update: bool = False) -> Optional[Account]:
         queryset =  Account.objects.filter(account_number=account_number).first()
         if select_for_update:
-            return queryset.select_for_update()
-        return queryset
+            return queryset.select_for_update().first()
+        return queryset.first()
 
 
 
