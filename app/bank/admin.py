fd7ee8068c818322ae6aa356f912283a871c4a93
antoniahp <a.herrerapuertas@gmail.com>
2024-09-11 15:37:57 +0200

    commission structure

--- app/bank/admin.py
+++ app/bank/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from bank.domain.account import Account
+from bank.domain.commisions_by_country import CommissionsByCountry
 from bank.domain.historic_movement import HistoricMovement
 from bank.domain.user import User
 
@@ -21,6 +22,16 @@ class HistoricMovementAdmin(admin.ModelAdmin):
         'category',
         'delta_amount',
     ]
+
+class CommissionsByCountryAdmin(admin.ModelAdmin):
+    list_display = [
+        'country',
+        'range_min',
+        'range_max',
+        'commission_percentage'
+    ]
+
 admin.site.register(User, UserAdmin)
 admin.site.register(Account, AccountAdmin)
 admin.site.register(HistoricMovement, HistoricMovementAdmin)
+admin.site.register(CommissionsByCountry, CommissionsByCountryAdmin)
--- app/bank/models.py
+++ app/bank/models.py
@@ -1,3 +1,4 @@
 from bank.domain.account import Account
 from bank.domain.user import User
 from bank.domain.historic_movement import HistoricMovement
+from bank.domain.commisions_by_country import CommissionsByCountry
